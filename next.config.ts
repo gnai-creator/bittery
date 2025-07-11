84531f81880601088ab9aa6bc896b95f69600b74
Felipe Maya Muniz <67922105+gnai-creator@users.noreply.github.com>
2025-07-11 10:17:28 -0300

    fix server build with walletconnect

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type {NextConfig} from 'next';
 
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Use the Node.js build of @walletconnect/keyvaluestorage on the server
+      config.resolve.alias["@walletconnect/keyvaluestorage"] = require.resolve(
+        "@walletconnect/keyvaluestorage/dist/index.cjs.js"
+      );
+    }
+    return config;
+  },
+};
 
 export default withNextIntl(nextConfig);
