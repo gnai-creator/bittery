b84359bea56a472289dd4e68eb5c830e4512244b
Felipe Maya Muniz <67922105+gnai-creator@users.noreply.github.com>
2025-07-11 03:20:59 -0300

    fix i18n import

--- i18n.ts
+++ i18n.ts
@@ -4,7 +4,17 @@ export const localePrefix = 'always';
 
 import {getRequestConfig} from 'next-intl/server';
 
+const messagesMap = {
+  en: () => import('./locales/en/common.json').then((m) => m.default),
+  pt: () => import('./locales/pt/common.json').then((m) => m.default),
+  es: () => import('./locales/es/common.json').then((m) => m.default),
+  fr: () => import('./locales/fr/common.json').then((m) => m.default),
+  sv: () => import('./locales/sv/common.json').then((m) => m.default),
+  de: () => import('./locales/de/common.json').then((m) => m.default),
+  ru: () => import('./locales/ru/common.json').then((m) => m.default),
+} as const;
+
 export default getRequestConfig(async ({locale}) => ({
-  messages: (await import(`./locales/${locale || defaultLocale}/common.json`)).default,
+  messages: await messagesMap[(locale ?? defaultLocale) as keyof typeof messagesMap](),
   locale: locale || defaultLocale,
 }));
