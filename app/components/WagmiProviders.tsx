be28e2adc0be89e7cd25c70cf571dccff915d0b4
Felipe Maya Muniz <67922105+gnai-creator@users.noreply.github.com>
2025-07-12 12:40:42 -0300

    fix walletconnect init

--- app/components/WagmiProviders.tsx
+++ app/components/WagmiProviders.tsx
@@ -2,25 +2,22 @@
 import { WagmiProvider, http } from "wagmi";
 import { polygon, sepolia } from "wagmi/chains";
 import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { ReactNode, useMemo, useState } from "react";
+import { ReactNode, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const wagmiConfig = getDefaultConfig({
+  appName: "Bittery",
+  projectId:
+    process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "bittery",
+  chains: [polygon, sepolia] as const,
+  transports: {
+    [polygon.id]: http(process.env.NEXT_PUBLIC_POLYGON_RPC_URL || ""),
+    [sepolia.id]: http(process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL || ""),
+  },
+});
 
 export default function WagmiProviders({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
-  const wagmiConfig = useMemo(
-    () =>
-      getDefaultConfig({
-        appName: "Bittery",
-        projectId:
-          process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "bittery",
-        chains: [polygon, sepolia] as const,
-        transports: {
-          [polygon.id]: http(process.env.NEXT_PUBLIC_POLYGON_RPC_URL || ""),
-          [sepolia.id]: http(process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL || ""),
-        },
-      }),
-    []
-  );
   return (
     <QueryClientProvider client={queryClient}>
       <WagmiProvider config={wagmiConfig}>
