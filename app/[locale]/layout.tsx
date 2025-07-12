5bb12631b0abf9540eae472689b5202ad10133db
Felipe Maya Muniz <67922105+gnai-creator@users.noreply.github.com>
2025-07-12 01:28:27 -0300

    feat: add navbar with withdraw menu

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import TermsBanner from "../components/TermsBanner";
 import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import "../globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import WagmiProviders from "../components/WagmiProviders";
@@ -68,6 +69,7 @@ export default async function RootLayout({
       >
         <WagmiProviders>
           <NextIntlClientProvider locale={locale} messages={messages}>
+            <Navbar />
             <main className="flex-grow">{children}</main>
             <Footer />
             <TermsBanner />
